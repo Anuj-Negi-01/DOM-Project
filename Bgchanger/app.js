1da9dd2e80cd02f56d8a798337fe2bb83545cc55
Anuj Negi <anujnegi@gmail.com>
2025-03-02 07:34:36 +0530

    complete bg changer

---
+++ Bgchanger/app.js
@@ -0,0 +1,36 @@
+const inputField = document.querySelector('#inputField')
+const createBtn = document.querySelector('.create-btn')
+const buttonGrid = document.querySelector('.button-grid')
+
+
+function createButton () {
+    // Minimum validation
+    if(inputField.value === ''){
+        alert('Please Enter a hexCode!' || inputField.value.length < 4 || inputField.value.length > 8)
+        inputField.value = ''
+        return 
+    }
+
+    const button = document.createElement('button')
+    button.className = 'grid-btn'
+    button.style.backgroundColor = `${inputField.value}`
+    button.innerText = inputField.value
+
+    button.addEventListener('click', () => {
+        if(document.body.style.backgroundColor !== 'rgb(255, 255, 255)'){
+            console.log('aaya hu');
+            document.body.style.backgroundColor = 'rgb(255, 255, 255)' 
+        }
+        else document.body.style.backgroundColor = `${button.innerText}`
+    })
+
+    buttonGrid.appendChild(button)
+
+    inputField.value = ''
+}
+
+createBtn.addEventListener('click', createButton)
+inputField.addEventListener('keydown', (e) => {
+    if(e.key !== 'Enter') return;
+    createButton()
+})
